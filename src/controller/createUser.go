16de76ffcdac9bf0abb005e126d2883314b0091e
Gisele C <giseledasilvac@gmail.com>
2025-01-27 10:14:44 -0300

    feat: adds logs by uber zap and fix validation on user_request

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,9 +1,42 @@
 package controller
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/giselescarvalho/CRUDemGo/src/configuration/logger"
+	"github.com/giselescarvalho/CRUDemGo/src/configuration/validation"
+	"github.com/giselescarvalho/CRUDemGo/src/controller/model/request"
+	"github.com/giselescarvalho/CRUDemGo/src/controller/model/response"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
 
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "createUser"),
+	)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to marashal object, error=%s", err, zap.String("journey", "createUser"))
+		errRest := validation.ValidateUserError(err)
+
+		c.JSON(errRest.Code, errRest)
+		return
+	}
+
+	fmt.Println("#######\n", userRequest)
+
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	logger.Info("User created sucessfully", zap.String("journey", "createUser"))
+
+	c.JSON(http.StatusOK, response)
 }
--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -10,7 +10,7 @@ func InitRoutes(r *gin.RouterGroup) {
 	//r.GET("/getUserById/:userId", func(c *gin.Context) {}, func(c *gin.Context) {})
 	r.GET("/getUserById/:userId", controller.FindUserById)
 	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
-	r.POST("/post/createUser", controller.CreateUser)
+	r.POST("/createUser", controller.CreateUser)
 	r.PUT("/updateUser/:userId", controller.UpdateUser)
 	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
 
